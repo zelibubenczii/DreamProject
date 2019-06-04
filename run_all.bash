fb9e1bb068e11c7cc728b20fa7cd2039079ab21f
zelibubenczii <zelibubenczii@mail.ru>
2019-06-04 02:15:55 +0200

    a module to plot contours has been added

--- run_all.bash
+++ run_all.bash
@@ -21,3 +21,11 @@ python3 Lib/postprocessor.py
 
 #changes cases file in each folder ends with "_NN" according to csv file in the "_CSV" folder
 Lib/VtkBuilder.bash
+
+#get contours of velocity Ux.jpg and U_y.jpg in each folder which ends with *"_NN" (neural network case)
+for D in *; do
+    if [ -d "${D}" && [ "${D}" == *"_NN" ]]; then
+        foamToVTK -case "${D}"
+        pvbatch Lib/paraView/getContoursUxUy.py
+    fi
+done
